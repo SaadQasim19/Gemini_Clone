1a061a465081f2c571ad58193de73fd978f1b5d7
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-08-11 12:09:26 +0500

    formateDateTime func

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,1 +1,8 @@
 // Utility functions for the Gemini AI Clone project
+
+
+// Function to format the current date and time
+function formatDateTime(date) {
+    const options = { hour: '2-digit', minute: '2-digit', hour12: true };
+    return date.toLocaleString('en-US', options);
+}
