aa0ed09f2a7a430d97e7e995c112acc6b607cc9e
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-08-12 19:38:25 +0500

    getGreeting func

--- src/js/utils.js
+++ src/js/utils.js
@@ -6,3 +6,13 @@ function formatDateTime(date) {
     const options = { hour: '2-digit', minute: '2-digit', hour12: true };
     return date.toLocaleString('en-US', options);
 }
+function getGreeting() {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+        return "Good morning";
+    } else if (hour < 18) {
+        return "Good afternoon";
+    } else {
+        return "Good evening";
+    }
+}
