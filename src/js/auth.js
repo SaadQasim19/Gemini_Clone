443f1f69a5c606d800f32f3490962d8d3db60762
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-08-12 19:45:24 +0500

    varibales for authentication

---
+++ src/js/auth.js
@@ -0,0 +1,15 @@
+
+const authForm = document.getElementById('authForm');
+const authButton = document.getElementById('authButton');
+const authButtonText = document.getElementById('authButtonText');
+const authSpinner = document.getElementById('authSpinner');
+const nameInput = document.getElementById('nameInput');
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
+const confirmPasswordInput = document.getElementById('confirmPasswordInput');
+const nameError = document.getElementById('nameError');
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passwordError');
+const confirmPasswordError = document.getElementById('confirmPasswordError');
+
+let isSignUp = false;
